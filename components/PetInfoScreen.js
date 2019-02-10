f3de144e7524aa9177220e75e03659ca5251f70d
Sarah Lee <sarahselee223@gmail.com>
2019-02-09 21:00:17 -0800

    add create pet's info

--- components/PetInfoScreen.js
+++ components/PetInfoScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import HeaderImage from './HeaderImage'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Button } from 'react-native-elements';
+import DatePicker from 'react-native-datepicker'
+import { Dropdown } from 'react-native-material-dropdown'
 
 export default class PetInfoScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      petSex: '',
+      petBirthday: '',
+
+      petBdOpen: false,
+    }
+}
+
   static navigationOptions = {
     headerTitle: <HeaderImage />,
     headerStyle: {
@@ -13,27 +25,49 @@ export default class PetInfoScreen extends React.Component {
     },
   };
   
+  HandlePicker = () => {
+    this.setState({
+      petSexPickerOpen: true,
+    })
+  }
   
   handleNext = () => this.props.navigation.navigate('PetInfoPage')
 
   render() {
+    let data = [{value: 'Girl'}, {value: 'Boy'}]
 
     return (
       <View style={styles.container}>
         <Text style={styles.titleText}>Create your pet's profile</Text>
           <View style={styles.inputContainer}>
+            
             <View style={styles.inputContainer2}>
               <Text style={styles.text}>Breed       </Text>
               <TextInput style={styles.textInput}></TextInput>
             </View>
-            <View style={styles.inputContainer2}>
-              <Text style={styles.text}>BirthDay  </Text>
-              <TextInput style={styles.textInput}></TextInput>
-            </View>
+
             <View style={styles.inputContainer2}>
               <Text style={styles.text}>Sex           </Text>
               <TextInput style={styles.textInput}></TextInput>
             </View>
+
+            <View style={styles.inputContainer2}>
+              <Text style={styles.text2}>BirthDay  </Text>
+              {/* <TextInput style={styles.textInput}></TextInput> */}
+                  <DatePicker
+                  showIcon = 'false'
+                  style={styles.datePickerStyle}
+                  date={this.state.petBirthday}
+                  mode="date"
+                  format="MM-DD-YYYY"
+                  minDate="01-01-2000"
+                  maxDate="12-31-2019"
+                  confirmBtnText="Confirm"
+                  cancelBtnText="Cancel"
+                  onDateChange={(date) => {this.setState({petBirthday: date})}}
+                  />
+            </View>
+
           </View>
           <Button title="Next" style={styles.nextBtn} onPress={this.handleNext}></Button>
       </View>
@@ -49,18 +83,26 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems:'center',
   },
+  datePickerStyle: {
+    width: 210, 
+    height: 25,
+    marginTop: 10,
+    marginBottom: '5%',
+    marginLeft: 13,
+  },
   inputContainer: {
     width: '80%',
-    justifyContent: "center",
     alignItems:'center',
   },
   inputContainer2: {
     flexDirection: "row",
+    marginTop: '2%',
+    marginBottom: '2%'
   },
   titleText: {
     marginTop: 30,
     marginBottom: 30,
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: 'bold',
     alignItems:'center',
     justifyContent: "center",
@@ -69,16 +111,25 @@ const styles = StyleSheet.create({
     marginTop: 20,
     fontSize: 20,
   },
+  text2: {
+    marginTop: 15,
+    fontSize: 20,
+  },
   textInput: {
     margin: 15,
     height: 30, 
-    fontSize: 25,
+    padding: 5,
+    fontSize: 18,
     width: 200,
     borderColor: 'gray', 
     borderWidth: 1
   },
+  inputPicker: {
+    width: 200,
+    fontSize: 18,
+  },
   nextBtn: {
-    marginTop: 30,
+    marginTop: '10%',
     width: 150,
   }
 });
