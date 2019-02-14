4ad2be693ffa150d6721952e5a5b1fa9ad448096
Sarah Lee <sarahselee223@gmail.com>
2019-02-14 14:11:48 -0800

    add function to apply photo to app

--- components/PetPhotoScreen.js
+++ components/PetPhotoScreen.js
@@ -5,13 +5,12 @@ import { ImagePicker, Permissions, Constants } from 'expo';
 export default class PetPhotoScreen extends Component {
   state = {
     result: null,
+    image: null,
   };
 
   askPermissionsAsync = async () => {
     await Permissions.askAsync(Permissions.CAMERA);
     await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    // you would probably do something to verify that permissions
-    // are actually granted, but I'm skipping that for brevity
   };
 
   useLibraryHandler = async () => {
@@ -22,6 +21,7 @@ export default class PetPhotoScreen extends Component {
       base64: false,
     });
     this.setState({ result });
+    this.setState({ image: result.uri });
   };
 
   useCameraHandler = async () => {
@@ -32,31 +32,40 @@ export default class PetPhotoScreen extends Component {
       base64: false,
     });
     this.setState({ result });
+    this.setState({ image: result.uri });
   };
+  
+  handleNext = () => this.props.navigation.navigate('PetInfoPage')
 
   render() {
+    let { image } = this.state;
     return (
+
         <ScrollView style={{flex: 1, flexDirection: 'column',}} contentContainerStyle={styles.container2}>
 
             <TouchableOpacity style={styles.roundImage} onPress={this.handleAddImage}>
-                <Image source={require('../assets/dog.png')}/> 
-                <Text style={styles.roundImageText}>+ Pet Image</Text>
+                {image ? <Image source={{ uri: image }} style={styles.image}/> 
+                    : (
+                    <View style={styles.dogImageContainer}>
+                        <Image source={require('../assets/dog.png')}/>
+                        <Text style={styles.roundImageText}>+ Pet Image</Text>
+                    </View> 
+                    )}
+
+
             </TouchableOpacity>
             <View style={styles.textContainer }>
-                <View style={{flexDirection: 'row', justifyContent:'center'}}>
-                    <Button title="Take a profile photo" onPress={this.useCameraHandler} />
-                    <Image source={require('../assets/camera.png')} style={{width:25, height:25}}/> 
-                </View>
-                <View style={{flexDirection: 'row', justifyContent:'center'}}>
-                    <Button
-                    title="Choose a photo from my album"
-                    onPress={this.useLibraryHandler}
-                    />
-                                        <Image source={require('../assets/files.png')} style={{width:25, height:25}}/> 
-                </View>
-                <Text style={styles.paragraph}>
-                {JSON.stringify(this.state.result)}
-                </Text>
+                <TouchableOpacity onPress={this.useCameraHandler} style={{flexDirection: 'column', justifyContent:'center', alignItems:'center', padding: 10}}>
+                    <Image source={require('../assets/camera.png')} style={{width:40, height:40, marginBottom: '2%'}}/> 
+                    <Text style={styles.roundImageText}> Take a profile photo </Text> 
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.useLibraryHandler} style={{flexDirection: 'column', justifyContent:'center', alignItems:'center', padding: 10}}>
+                    <Image source={require('../assets/files.png')} style={{width:40, height:40, marginBottom: '2%'}}/> 
+                    <Text style={styles.roundImageText}>Choose a photo from my album</Text>
+                </TouchableOpacity>
+                <Button title="Save this photo" onPress={() => this.handleNext(image)}>
+                
+                </Button>
             </View>
         </ScrollView>
         
@@ -72,15 +81,9 @@ const styles = StyleSheet.create({
 
   },
   container2: {
-    marginTop: '35%',
+    marginTop: '50%',
     justifyContent:'center',
     alignItems:'center',
-  },
-  paragraph: {
-    marginHorizontal: 15,
-    marginTop: 30,
-    fontSize: 18,
-    color: '#34495e',
   },
   roundImage: {
     alignItems: 'center',
@@ -89,6 +92,19 @@ const styles = StyleSheet.create({
     width:200,
     height:200,
     borderRadius:100,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  dogImageContainer:{
+    flexDirection: "column", 
+    alignItems:'center', 
+    justifyContent:'center',
+  },
+  image: {
+    resizeMode: "stretch",
+    height: 200,
+    width: 200,
+    borderRadius: 100,
     alignItems:'center',
     justifyContent:'center',
   },
