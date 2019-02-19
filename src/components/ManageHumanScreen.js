370c3df1ebed5d00f7a6af3270a7af03da36d46e
Sarah Lee <sarahselee223@gmail.com>
2019-02-18 21:15:36 -0800

    add profile and invitation page

---
+++ src/components/ManageHumanScreen.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, TextInput, Image, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import BottomNav from './BottomNav'
+import HeaderImage from './HeaderImage'
+import styles from '../styling/ManageHumanScreen'
+import { Button } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { saveName } from '../action/pets'
+
+class ManageHumanScreen extends React.Component {
+  state = {
+    openErrorMsg: false,
+    openProfile: false,
+    openInvitation: false
+  }
+
+  static navigationOptions = {
+    headerTitle: <HeaderImage />,
+    headerStyle: {
+      height: 100,
+      fontWeight: 'bold',
+    },
+  };
+
+  handleNext = () => {
+    if(!this.state.humanEmail || !this.state.humanPw){ 
+      return this.setState({ openErrorMsg: true })
+    }
+    this.props.navigation.navigate('DailySchedulePage')
+  }
+
+  render() {
+  const image = this.props.humansData.humanImage
+  const name = this.props.humansData.humanName
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+
+        <View style={styles.contentsContainer}>
+            {image ? (
+            <View style={styles.roundImage} onPress={this.handleAddImage}>
+                <Image style={styles.image} source={{ uri: image }} /> 
+            </View>
+            ) : (
+            <TouchableOpacity style={styles.roundImage} onPress={this.handleAddImage}>
+                <Image style={styles.image2} source={require('../../assets/happy.png')}/> 
+                <Text style={styles.roundImageText}>+ Human Image</Text>
+            </TouchableOpacity>
+            )}
+        {this.state.openProfile? (
+            <View style={styles.profileContainer}>
+                <Text style={styles.profileText1}>Sarah</Text>
+                <Text style={styles.profileText}>Title</Text>
+                <Text style={styles.profileText}>Email</Text>
+                <Text style={styles.profileText}>Phone</Text>
+            </View>
+        ): null}
+            
+        {this.state.openInvitation? (
+        <View style={styles.profileContainer}>
+            <Text style={styles.profileText}>Name</Text>
+            <TextInput style={styles.textInput1} onChangeText={(human2Name) => this.setState({human2Name})}></TextInput>
+            <Text style={styles.profileText}>Email</Text>
+            <TextInput style={styles.textInput2} onChangeText={(human2Email) => this.setState({human2Email})}></TextInput>
+            <Button style={styles.btn} title="Send Invitation" onPress={()=>{this.setState({ openInvitation: !this.state.openInvitation, openProfile: false})}}></Button>
+        </View>
+        ): null}
+
+            <View style={styles.btnContainer}>
+                <Button style={styles.btn} type="outline" title="Your Human's profile" onPress={()=>{this.setState({ openProfile: !this.state.openProfile, openInvitation: false})}}></Button>
+                <Button style={styles.btn} type="outline" title="Invite More Human"onPress={()=>{this.setState({ openInvitation: !this.state.openInvitation, openProfile: false})}}></Button>
+            </View>
+
+        </View>
+        <View style={styles.bottomNavContainer}>
+          <BottomNav />
+        </View>
+   
+      </View>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({
+    petsData: state.petsData,
+    humansData: state.humansData
+  })
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ saveName }, dispatch)
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageHumanScreen)
+
---
+++ src/styling/ManageHumanScreen.js
@@ -0,0 +1,101 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+    },
+    inputContainer: {
+      justifyContent:'center',
+    },
+    profileContainer: {
+        marginTop: "4%",
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    bottomNavContainer: {
+      padding: 3,
+      fontSize: 30,
+      justifyContent:'center',
+      height: '13%',
+    },
+    contentsContainer: {
+      height: '85%',
+      alignItems:'center',
+      marginTop: 20
+    },
+    roundImage: {
+        alignItems: 'center',
+        borderWidth:1,
+        borderColor:'rgba(0,0,0,0.8)',
+        width:120,
+        height:120,
+        borderRadius:60,
+        justifyContent:'center',
+    },
+    image: {
+        resizeMode: "stretch",
+        height: 120,
+        width: 120,
+        borderRadius: 60
+    },
+    image2: {
+        height: 35,
+        width: 35,
+        marginTop: "10%"
+    },
+    roundImageText:{
+      justifyContent:'center',
+      fontSize: 13
+    },
+    text: {
+      marginTop: 20,
+      fontSize: 15,
+      marginLeft: 10,
+    },
+    profileText:{
+       fontSize: 17,
+       margin: 5
+    },
+    profileText1:{
+        fontSize: 20,
+        margin: 5
+     },
+    textInput1: {
+      alignItems:'center',
+      justifyContent:'center',
+      margin: 10,
+      height: 30, 
+      fontSize: 17,
+      width: 150,
+      borderColor: 'gray', 
+      borderWidth: 1
+    },
+    textInput2: {
+        alignItems:'center',
+        justifyContent:'center',
+        margin: 10,
+        height: 30, 
+        fontSize: 17,
+        width: 230,
+        borderColor: 'gray', 
+        borderWidth: 1
+      },
+    
+    btnContainer:{
+        width: 200,
+        margin: "3%"
+    },
+    btn:{
+        margin: "3%"
+    },
+    nextBtn: {
+      marginTop: 30,
+      width: 150,
+      alignItems:'center',
+      justifyContent:'center',
+    }
+  })
+  
+  export default styles  
